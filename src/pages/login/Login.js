184be3a91f146558958555075854f301a63f4d13
Mladen <130111025+k0jiccc@users.noreply.github.com>
2023-11-29 21:41:12 +0100

    boze pomozi

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -4,19 +4,47 @@ import './Login.css';
 
 const Login = () => {
   return (
-    <div className="login">
-      <h2>Login</h2>
-      <form>
-        <label>
-          Email:
-          <input type="email" name="email" />
-        </label>
-        <label>
-          Password:
-          <input type="password" name="password" />
-        </label>
-        <input type="submit" value="Login" />
-      </form>
+    <div className="container-contact3">
+      <div className="wrap-contact3">
+        <form className="contact3-form validate-form">
+          <span className="contact3-form-title">
+            Contact Us
+          </span>
+          <div className="wrap-contact3-form-radio">
+            <div className="contact3-form-radio m-r-42">
+              <input
+                className="input-radio3"
+                id="radio1"
+                type="radio"
+                name="choice"
+                value="say-hi"
+                checked
+              />
+              <label className="label-radio3" htmlFor="radio1">
+                Say Hi
+              </label>
+            </div>
+            <div className="contact3-form-radio">
+              <input
+                className="input-radio3"
+                id="radio2"
+                type="radio"
+                name="choice"
+                value="get-quote"
+              />
+              <label className="label-radio3" htmlFor="radio2">
+                Get a Quote
+              </label>
+            </div>
+          </div>
+          {/* ... other form elements ... */}
+          <div className="container-contact3-form-btn">
+            <button className="contact3-form-btn">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
